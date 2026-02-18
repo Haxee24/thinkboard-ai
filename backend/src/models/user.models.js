6ef5c3b273f03c8580f7293d119127d969ec22d1
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-19 02:00:59 +0530

    made minor changes to User model and Notes Model

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,7 +1,17 @@
 import mongoose, {Schema} from "mongoose";
 
 const userSchema = new Schema({
-    name: {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 30,
+        match: /^[a-zA-Z0-9]+$/,
+        lowercase: true,
+        trim: true
+    },
+    fullname: {
         type: String,
         required: true
     },
@@ -9,16 +19,19 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+        match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        trim: true,
+        lowercase: true
     },
     password: {
         type: String,
         required: true
     },
-    notes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Note"
-    }]
+    notes: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Note",
+        default: []
+    }
 }, {timestamps: true});
 
 export default mongoose.model("User", userSchema);
