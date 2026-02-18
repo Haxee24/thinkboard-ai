c649af1eca26e343a2afbef3452b4c14449f7e9d
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-18 12:51:07 +0530

    Configured dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import app from './server.js';
 
 const PORT = process.env.PORT || 3001;
+console.log(`PORT: ${PORT}`);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
@@ -8,4 +9,5 @@ app.listen(PORT, () => {
 
 app.get('/', (_, res) => {
     res.send('Hello World!');
-})
+});
+
