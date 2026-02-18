02a97f2e0bef0c3960d15073237ffb5cefe71113
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-18 12:44:40 +0530

    created gitignore and initialized express server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,11 @@
-import app from './app.js';
+import app from './server.js';
 
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/', (_, res) => {
+    res.send('Hello World!');
+})
