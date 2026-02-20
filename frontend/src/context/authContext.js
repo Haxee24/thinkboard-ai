0d3fafd8c2693390509355e9d7e6984223908a61
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-20 12:14:04 +0530

    improved authProvider

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -1,15 +1,31 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({children}) {
     const [user, setUser] = useState(null);
 
+    const login = (userData) => {
+        setUser(userData);
+    }
+    
+    const logoout = async () => {
+        await fetch('http://localhost:5000/api/logout', {
+            method: 'POST',
+            credentials: 'include',
+        });
+        setUser(null);
+    }
+
     return (
         <AuthContext.Provider value={{user}}>
             {children}
         </AuthContext.Provider>
     )
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext);
 }
 
 export default AuthContext;
